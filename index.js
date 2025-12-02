b0b3f36c3eea81662a21396fc71e8387263ac816
Evyatar Yotvat <Evyatar431@gmail.com>
2025-12-02 23:49:50 +0200

    Commit 3 - updated server files

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const express = require("express");
-const morgan = require("morgan");
-const bodyParser = require("body-parser");
+import express from "express";
+import morgan from "morgan";
+import bodyParser from "body-parser";
 
 const app = express();
 
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 app.post("/api/cars", (req, res) => {
     const { brand, model } = req.body;
 
-    res.send(`You chose ${brand} ${model}. `);
+    res.send(`You chose ${brand} ${model}.`);
 });
 
 app.listen(3000, () => {
