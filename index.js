66b7a9f2c1d449a5327f71bb1b4f34251dae8f70
Evyatar Yotvat <Evyatar431@gmail.com>
2025-12-03 00:56:32 +0200

    Commit 4 - fixed client-server communication and resolved 500 response error

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
-import express from "express";
-import morgan from "morgan";
-import bodyParser from "body-parser";
+const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
 
+app.use(cors());
 app.use(morgan("dev"));
+
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.post("/api/cars", (req, res) => {
     const { brand, model } = req.body;
-
     res.send(`You chose ${brand} ${model}.`);
 });
 
 app.listen(3000, () => {
-    console.log("Server running on http://localhost:3000");
+    console.log("Server is running on port 3000");
 });
